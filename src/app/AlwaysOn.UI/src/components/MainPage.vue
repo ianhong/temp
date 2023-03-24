dae4baea075a4060e132da574fa39005b146a04d
Chris Beron <cberon@costco.com>
2023-03-24 13:54:51 -0700

    added minimal costo stylings

--- src/app/AlwaysOn.UI/src/components/MainPage.vue
+++ src/app/AlwaysOn.UI/src/components/MainPage.vue
@@ -1,13 +1,17 @@
 <template>
-  <div>
-   <h1>😎 Welcome,</h1>
-   <p>... to Azure Mission-Critical! This is a reference implementation of a community driven project designed to demo and document the process, requirements and design decisions to setup a highly scalable and available application in Microsoft Azure.</p>
+    <div id="header" class="c1000">
+        <a class="header-logo" href="https://www.costco.com/" id="WC_CachedHeaderDisplay_Link_2a"><img src="/img/logo.png" alt="USBC"></a>
+    </div>
+    <div>
+        <h1>😎 Welcome,</h1>
+        <p>... to Azure Mission-Critical! This is a reference implementation of a community driven project designed to demo and document the process, requirements and design decisions to setup a highly scalable and available application in Microsoft Azure.</p>
 
-   <a href="https://github.com/Azure/Mission-Critical"><img src="/img/logo.png" title="Mission-Critical logo" style="width: 50%" /></a>
+        <p>You can find more details <a href="https://github.com/Azure/Mission-Critical-Connected">on GitHub</a>.</p>
 
-   <p>You can find more details <a href="https://github.com/Azure/Mission-Critical-Connected">on GitHub</a>.</p>
-
-  </div>
+    </div>
+    <div id="footer-bottom" class="text-center">
+        <p class="copyright">&#169; 1998 &#8212; <span id="copyright-year">2023</span> Costco Wholesale Corporation. <span class="rights">All rights reserved.</span></p>
+    </div>
 </template>
 
 <script>
