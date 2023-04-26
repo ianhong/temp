bf384e0c82851f1a11f8c9e89854a06906491fb4
Chris Beron <cberon@costco.com>
2023-04-26 11:13:56 -0700

    migrated azure service connection out of pipeline and into env config files

--- src/app/Costco.ECom.API.InventoryAvailability/Program.cs
+++ src/app/Costco.ECom.API.InventoryAvailability/Program.cs
@@ -2,6 +2,7 @@
 // Copyright (c) Costco Wholesale. All rights reserved.
 // </copyright>
 // ----------------------------------------------------------------------
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
 using Serilog;
 
 namespace Costco.ECom.API.InventoryAvailability
@@ -13,6 +14,7 @@ namespace Costco.ECom.API.InventoryAvailability
             try
             {
                 Log.Information($"Starting web host for {nameof(InventoryAvailability)}");
+
                 CreateHostBuilder(args).Build().Run();
                 return 0;
             }
@@ -29,7 +31,7 @@ namespace Costco.ECom.API.InventoryAvailability
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-            .ConfigureAppConfiguration((context, config) =>
+          /*  .ConfigureAppConfiguration((context, config) =>
             {
                 // Load values from k8s CSI Key Vault driver mount point
                 config.AddKeyPerFile(directoryPath: "/mnt/secrets-store/", optional: true, reloadOnChange: true);
@@ -44,11 +46,36 @@ namespace Costco.ECom.API.InventoryAvailability
                                             outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} {Level:u3}] {Message:lj} {Properties:j}{NewLine}{Exception}")
                                     .WriteTo.ApplicationInsights(builtConfig[SysConfiguration.ApplicationInsightsConnStringKeyName], TelemetryConverter.Traces)
                                     .CreateLogger();
-            })
-            .UseSerilog()
+            }) */
             .ConfigureWebHostDefaults(webBuilder =>
             {
-                webBuilder.UseStartup<Startup>();
-            });
+                webBuilder
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var settings = config.Build();
+                    config.AddAzureAppConfiguration(options =>
+                    options
+                        .Connect(settings.GetConnectionString("AppConfig"))
+                        // Load configuration values with no label
+                        .Select("InventoryService:*", LabelFilter.Null)
+                        // Override with any configuration values specific to current hosting env
+                        .Select("InventoryService:*", hostingContext.HostingEnvironment.EnvironmentName)
+                        // Configure to reload configuration if the registered sentinel key is modified
+                        .ConfigureRefresh(refreshOptions =>
+                            refreshOptions.Register("InventoryService:Settings:Sentinel", refreshAll: true))
+                    );
+
+                    // TODO: Transition Serilog AppInsights sink to use the connection string instead of Instrumentation Key, once that is fully supported
+                    Log.Logger = new LoggerConfiguration()
+                                        .ReadFrom.Configuration(settings)
+                                        .Enrich.FromLogContext()
+                                        .WriteTo.Console(
+                                                outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} {Level:u3}] {Message:lj} {Properties:j}{NewLine}{Exception}")
+                                        .WriteTo.ApplicationInsights(settings[SysConfiguration.ApplicationInsightsConnStringKeyName], TelemetryConverter.Traces)
+                                        .CreateLogger();
+                })
+                .UseStartup<Startup>();
+            })
+            .UseSerilog();
     }
 }
